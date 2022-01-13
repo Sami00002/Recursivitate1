c70715c8f9ef1811afbe4fa4388f3bd07332525c
Sami00002 <samuel.bodogan@emanuel.ro>
2022-01-13 15:37:09 +0200

    Add project files.

---
+++ Recursivitate1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 1, 2, 3, 4, 5, 6 , 7 , 8 , 9 , 10 , 11 , 12};
+            Recursiv.ParcurgereVector(vector, vector.Length);
+        }
+    }
+}
---
+++ Recursivitate1/Recursiv.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate1
+{
+    public static class Recursiv
+    {
+        public static void ParcurgereVector(int[] vector, int n)
+        {
+            if (n == 1)
+                Console.WriteLine(vector[n-1]);
+            else
+            {
+                
+                ParcurgereVector(vector, n - 1);
+                Console.WriteLine(vector[n-1]);
+            }
+        }
+    }
+}
